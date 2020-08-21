adcfd94b99652b3bf848211967b080e544eef94f
XisHungry <60353325+XisHungry@users.noreply.github.com>
2020-08-21 10:35:07 +0700

    Update GPS.py

--- GPS.py
+++ GPS.py
@@ -5,7 +5,7 @@ with serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1) as ser:
 			line = ser.readline().decode('ascii', errors = 'replace')
 			lines = line.split(",")
 			if lines[0] == "$GPRMC":
-				text_file = open("GPS.txt", "w")
+				text_file = open("utils/GPS.txt", "w")
 				text_file.write("Latitude=" + lines[3] + "," + "Longitude=" + lines[5])
 				text_file.close()
 		except KeyboardInterrupt:
